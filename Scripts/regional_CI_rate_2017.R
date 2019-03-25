3b7355e2dc24ca317741ebeabec145abc768790d
OD <owendnl@gmail.com>
2019-03-25 07:57:50 +0000

    Separated crown/mags plots. Added note about CIs.

--- Scripts/regional_CI_rate_2017.R
+++ Scripts/regional_CI_rate_2017.R
@@ -21,8 +21,9 @@
 #    - Offence group, so do not consider individual offence codes.
 #    - Outcome, so do not consider further details of sentences, or sentence lengths.
 #
-# Credible intervals are derived based on each individual binomial rate having the 
-# Jeffrey's prior, the Beta(1/2,1/2) prior.
+# Confidence intervals are derived using Jeffrey's method for binomial 
+# confidence intervals; this is equivalent to the Bayesian credible interval that
+# is obtained from using the Binomial Jeffrey's prior, the Beta(1/2,1/2) distribution.
 
 # Read in prepared master data.
 source("./Scripts/load_clean_data.R")
@@ -42,16 +43,34 @@ region_data <- data %>%
   summarise(
     outcome_IC = sum(count * (outcome == "15: Immediate custody")),
     outcome_not_IC = sum(count * (outcome != "15: Immediate custody")),
-    count = sum(count)    
-    # NOT RUN: count_check = outcome_IC + outcome_not_IC
-  )
+    count = sum(count)
+    # NOT RUN: count_check = outcome_IC + outcome_not_IC - count
+  ) %>%
+  ungroup()
 
-# Add posterior Beta(alpha,beta) parameters in line with a Beta(1/2,1/2) prior.
+# Create summary data frame by region and court type.
+region_summary <- region_data %>%
+  group_by(region, country, court) %>%
+  summarise(
+    count = sum(count),
+    outcome_IC = sum(outcome_IC),
+    IC_rate = outcome_IC / count
+  ) %>%
+  ungroup()
+
+# 
+# => Below we generate confidence intervals for the regional summaries.
+#
+
+# Calculate Bayesian posterior parameters in line with the Jeffreys prior. 
 region_data <- region_data %>%
   mutate(
     alpha = outcome_IC + 1/2,
     beta  = outcome_not_IC + 1/2
   )
+
+# Set random number seed.
+set.seed(17880205)
 
 # Generate samples from each posterior distribution; we use 1000 samples per stratum.
 samples <- region_data %>% crossing(data_frame(sample_id = 1:1000)) %>%
@@ -67,7 +86,8 @@ region_samples <- samples %>%
     outcome_IC = sum(outcome_IC),
     count = sum(count),
     IC_rate = outcome_IC / count
-  ) %>% ungroup()
+  ) %>%
+  ungroup()
 
 # Obtain credible intervals for total CI rate for each region.
 region_CredInt <- region_samples %>%
@@ -78,12 +98,46 @@ region_CredInt <- region_samples %>%
     hgh95 = quantile(IC_rate, 0.975)
   ) %>% ungroup()
 
-# Plot CI rates by region.
-region_CredInt %>%
-  mutate(country = ifelse(region == "Wales/Cymru", "Wales", "England")) %>%
-  filter(region != "Specialist") %>%
-  ggplot(aes(mid50, region, color = country)) + geom_point() + 
+# Join credible intervals with the summary data frame.
+region_summary <- left_join(region_summary, region_CredInt)
+
+#
+# => Below we generate plots of the confidence regions.
+#
+
+# Crown court plot.
+region_summary %>%
+  filter(court == "01: Crown Court",
+         region != "Specialist") %>%
+  mutate(
+    region = fct_reorder(region, desc(IC_rate))
+  ) %>%
+  ggplot(aes(IC_rate, region, color = country)) + geom_point() + 
+  geom_point(aes(mid50, region), shape = 3) +
   geom_errorbarh(aes(xmin = low95, xmax = hgh95)) +
-  facet_grid(cols = vars(court), scales = "free_x") +
-  scale_color_manual(values = c("Wales" = "#960a0a", "England" = "#e5a40d"))
+  scale_color_manual(values = c("Wales" = "#F2300F", "England" = "#35274A")) +
+  scale_x_continuous(labels = scales::percent) +
+  ggtitle("Crown Court Immediate Custody Rates and 95% Credible Intervals") +
+  xlab("Immediate Custody Rate") + ylab("Region")
+  
+# Crown court plot.
+region_summary %>%
+  filter(court == "02: Magistrates Court",
+         region != "Specialist") %>%
+  mutate(
+    region = fct_reorder(region, desc(IC_rate))
+  ) %>%
+  ggplot(aes(IC_rate, region, color = country)) + geom_point() + 
+  geom_point(aes(mid50, region), shape = 3) +
+  geom_errorbarh(aes(xmin = low95, xmax = hgh95)) +
+  scale_color_manual(values = c("Wales" = "#F2300F", "England" = "#35274A")) +
+  scale_x_continuous(labels = scales::percent) +
+  ggtitle("Magistrates Court Immediate Custody Rates and 95% Credible Intervals") +
+  xlab("Immediate Custody Rate") + ylab("Region")
+
+# Note: Both plots show a consistent mis-estimates of the center of the credible region,
+# in relation to the observed IC rate: in the case of the Crown data estimates are consistently
+# below observed, whilst in the magistrates court this is reversed.
+#
+# This could be improved by using more informative priors than the Jeffreys prior.
 
